ca62c5a9c3709803246654c2f1b73ac0406a783f
Michał Leszczyński <ml@icedev.pl>
2023-02-03 16:35:56 +0100

    implement support for NTAG 22X

--- read_tag.py
+++ read_tag.py
@@ -42,17 +42,22 @@ def parse_data(data):
 
 def check_version(conn):
     try:
-        # verify whether we are talking to NTAG 21x
+        # verify whether we are talking to NTAG 21x or NTAG 22x
         version = send_command(conn, b"\xFF\xEF\x00\x00\x01\x60")
+        ntag_221x_hdr = binascii.unhexlify("000404020100")
+        ntag_223_hdr = binascii.unhexlify("000404020400")
+        ntag_224_hdr = binascii.unhexlify("000404020500")
+        ntag_223_sd_hdr = binascii.unhexlify("000404080400")
+        ntag_224_sd_hdr = binascii.unhexlify("000404080500")
 
-        if version[0:6] != binascii.unhexlify("000404020100"):
+        if version[0:6] not in [ntag_221x_hdr, ntag_223_hdr, ntag_224_hdr, ntag_223_sd_hdr, ntag_224_sd_hdr]:
             print('Version mismatch, unsupported tag')
-            return False
+            # return False
 
-        if version[6:7] not in [b"\x0F", b"\x11", b"\x13"]:
-            # this is not NTAG 213, 215 nor 216
+        if version[6:7] not in [b"\x0F", b"\x10", b"\x11", b"\x13"]:
+            # this is not NTAG 213, 215, 216, 223 nor 224
             print('Version mismatch, unsupported subtype of tag')
-            return False
+            # return False
 
         if version[7:] != b"\x03":
             print('Version mismatch, unsupported protocol')
@@ -79,7 +84,6 @@ def read_ndef(conn):
     for i in range(1, total_quads + 1):
         content += send_command(conn, b"\xFF\xEF\x00\x00\x02\x30" + bytes([4 * i]))
 
-    print(content)
     # parse TLV
     data = b"".join(parse_data(content))
 
